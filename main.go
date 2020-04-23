6a8b303e509634a3cfba302f5c0f8735a71ca6b2
Lionel Félicité <deogracia@free.fr>
2020-04-23 17:04:34 +0200

    init code with spf13/cobra

---
+++ main.go
@@ -0,0 +1,15 @@
+/*
+Copyright © 2020 Lionel Félicité <deogracia@free.fr>
+This file is licensed under the BSD 3-Clause Clear License.
+The full text can also be found:
+  * in the LICENSE file at the root directory of this project
+  * at https://spdx.org/licenses/BSD-3-Clause-Clear.html
+
+*/
+package main
+
+import "github.com/deogracia/jntpdn/cmd"
+
+func main() {
+	cmd.Execute()
+}
