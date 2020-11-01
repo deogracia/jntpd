5b0dca5b484e1ee90104142a8756cbbbb9de8ba0
Lionel Félicité <deogracia@free.fr>
2020-11-01 21:37:13 +0100

    basic test on docs
    refs #34

--- cmd/docs_test.go
+++ cmd/docs_test.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"bytes"
+	"os"
 	"testing"
 )
 
@@ -10,8 +11,14 @@ func TestDocsAction(t *testing.T) {
 	tempDir := t.TempDir()
 	out := bytes.NewBufferString("")
 
-	err := docsAction(out, "/tmp/jntpdn-test")
+	err := docsAction(out, tempDir)
 	if err != nil {
 		t.Fatalf("Expected \"%s\"; got \"%s\". tempDir = \"%s\"", "nil", out.String(), tempDir)
 	}
+
+	_, err = os.Stat(tempDir)
+	if os.IsNotExist(err) {
+		t.Fatalf("\"%s\" doesn't exist.", tempDir)
+	}
+
 }
